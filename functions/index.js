aa418a7fd4336e7b5bf6c728c3709a5ab3aa4537
Todd Albert <todd@bocacode.com>
2022-11-16 14:05:33 -0500

    ready to go

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { getAllPhotos, addNewPhoto } from "./src/photos.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json()); // any time we do anything other than .get
+
+// routes here...
+app.get('/photos', getAllPhotos);
+app.post('/photos', addNewPhoto);
+
+export const api = functions.https.onRequest(app);
